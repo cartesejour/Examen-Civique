2d137eb946c325ffdb72940659631b7c702e627a
cartesejour <contactcartesejour@gmail.com>
2026-04-30 14:16:32 +0200

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,20 +1,21 @@
 const API = {
-    async loadBaseFR() {
+    async loadBaseFR(lvl) {
         try {
-            const res = await fetch('data/questions_fr.json');
+            // lvl.toLowerCase() donnera 'csp' ou 'cr'
+            const res = await fetch(`data/questions_${lvl.toLowerCase()}_fr.json`);
             return await res.json();
         } catch (e) {
             console.error("Erreur Base FR", e);
             return [];
         }
     },
-    async loadHelp(lang) {
+    async loadHelp(lvl, lang) {
         if (lang === "none") return [];
         try {
-            const res = await fetch(`data/questions_${lang}.json`);
+            const res = await fetch(`data/questions_${lvl.toLowerCase()}_${lang}.json`);
             return await res.json();
         } catch (e) {
-            console.warn("Traduction indisponible", e);
+            console.warn(`Traduction ${lang} indisponible pour le niveau ${lvl}`, e);
             return [];
         }
     }
